2be64a5e0fce3e2c4566868b3097b81d9fa0ea33
vishalcoder0912 <vishal.kumar.1304200504@gmail.com>
2026-09-26 13:34:52 +0530

    refactor(server): drop server-only marker to allow client RPC proxies

--- src/lib/server/features/settings.server.ts
+++ src/lib/server/features/settings.server.ts
@@ -1,5 +1,3 @@
-import "@tanstack/react-start/server-only";
-
 import { createServerFn } from "@tanstack/react-start";
 import { requireAdmin, requireUser } from "../auth";
 import { settingsPatchSchema } from "../contracts";
